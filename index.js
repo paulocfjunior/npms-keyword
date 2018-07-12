5697890f490446109ca2eeb6ddb61e2f788fe82f
paulocfjunior <paulo.junior@gs.liferay.com>
2018-07-12 09:12:45 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var got = require('got');
+var registryUrl = require('registry-url');
+var Promise = require('pinkie-promise');
+
+function get(keyword, level) {
+    if (typeof keyword !== 'string') {
+        return Promise.reject(new TypeError('Keyword must be a string'));
+    }
+
+    keyword = encodeURIComponent(keyword);
+
+    var url = "https://api.npms.io/v2/search?q=" + keyword;
+
+    return got(url, {json: true}).then(function (res) {
+        return res.body.results;
+    });
+}
+
+module.exports = function (keyword) {
+    return get(keyword, 3).then(function (data) {
+        return data.map(function (el) {
+            return {
+                name: el.package.name,
+                description: el.package.description
+            };
+        });
+    });
+};
+
+module.exports.names = function (keyword) {
+    return get(keyword, 2).then(function (data) {
+        return data.map(function (el) {
+            return el.package.name;
+        });
+    });
+};
+
+module.exports.count = function (keyword) {
+    return get(keyword, 1).then(function (data) {
+        return data[0] ? data[0].value : 0;
+    });
+};
